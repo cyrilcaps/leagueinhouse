812a88c344a91e2437eeb82d1321a81aafb30d65
Raphib737 <raph737@bu.edu>
2019-03-16 03:24:58 -0400

    ADD: Champion square images, and added to component

--- inhouse_analyzer/client/src/components/OverviewProfiles.js
+++ inhouse_analyzer/client/src/components/OverviewProfiles.js
@@ -1,7 +1,15 @@
 import '../App.css';
-
 import React, {Component} from 'react';
 
+const importAll = require => require.keys().reduce((acc, next) => {
+  acc[next.replace('./', '')] = require(next);
+  return acc;
+}, {});
+
+const images =
+    importAll(require.context('./images/champion_squares', false, /\.(png)$/));
+
+console.log(images);
 class SummonerProfiles extends Component {
   constructor(props) {
     super();
@@ -9,15 +17,39 @@ class SummonerProfiles extends Component {
 
   render() {
     let d = this.props['data']['summoner'];
-    console.log(d)
-    return (<div className = 'userProfiles'><div className = 'OsummName'>{
-        d['summoner']}<
+    var summoner = d['summoner'], won = d['won'], lost = d['lost'],
+        g_p = d['games played'], champs = d['most played champs'],
+        roles = d['most played roles'], winRate = d['win rate'];
+
+    var champ_list = [];
+    for (var i in champs) {
+      let source = champs[i][0] + '.png'
+      champ_list.push(<img className = 'Ochamps' src={images[source]}></img>)
+      if (champ_list.length > 2) {
+        break;
+      }
+    }
+
+    var role_list = [];
+    for (var k in roles) {
+      role_list.push(roles[k][0]);
+      if (role_list.length > 2) {
+        break;
+      }
+    }
+
+
+    return (
+        <div className = 'userProfiles'><div className = 'OsummName'>{summoner}<
             /div>
-        <div className = 'Orecord'>Won : {d['won']}<br></br>
-                Lost: {d['lost']}<br>
-            </br>Games played: {d['games played']}<br></br>Win Rate:
-                {d['win rate']}</div>
-        </div>)
+        <div className = 'Orecord'>Won : {won}<br></br>Lost:
+            {lost}<br>
+        </br> Win Rate:
+                    {winRate}<br></br>Roles:
+            {role_list.toString().toLowerCase()}<
+                /div>
+            <div className = 'divChamps'>Played Champs:<br></br>{[champ_list]}</div>
+  < /div>)
   }
 }
 

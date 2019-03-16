5eec8eca0aea9579a5b0257c3f70be56e69da47f
Raphib737 <raph737@bu.edu>
2019-03-15 23:19:20 -0400

    ADD: navbar to main app component

--- inhouse_analyzer/client/src/App.js
+++ inhouse_analyzer/client/src/App.js
@@ -7,6 +7,10 @@ class App extends Component {
     response: '',
     post: '',
     responseToPost: '',
+    active: 'overview',
+    summoners: false,
+    overview: true,
+    champions: false
   };
   componentDidMount() {
     this.callApi()
@@ -20,19 +24,39 @@ class App extends Component {
     return body;
   };
 
+  activateState(e) {
+    let activeState = e.target.text.toLowerCase();
+    let newState = {active: activeState};
+    let states = ['summoners', 'champions', 'overview'];
+    for (var i in states) {
+      if (activeState === states[i]) {
+        newState[states[i]] = true;
+      } else {
+        newState[states[i]] = false;
+      }
+    }
+    this.setState(newState)
+  };
+
   render() {
-    return (<NavBar />);
+    let oClass = this.state.overview ? 'active' : '';
+    let sClass = this.state.summoners ? 'active' : '';
+    let cClass = this.state.champions ? 'active' : '';
+    return (
+        <div className = 'test'><ul className = 'navbar'><li>
+        <a className = {oClass} href = '#' onClick = {this.activateState.bind(
+             this)}>Overview</a>
+        </li><li>
+        <a className = {sClass} href = '#Summoners' onClick =
+             {this.activateState.bind(this)}>Summoners</a>
+        </li><li>
+        <a className = {cClass} href = '#Champions' onClick =
+             {this.activateState.bind(this)}>Champions</a>
+        </li>
+        </ul></div>);
   }
 }
 
-class NavBar extends Component {
-  state = {active: 'overview'};
 
-  render() {
-    return (<div className = 'nav'><ul className = 'navbar'><li>
-            <a href = '#'>Overview</a></li><li>
-            <a href = '#Summoners'>Summoners</a></li><li>
-            <a href = '#Champions'>Champions</a></li></ul></div>);
-  };
-}
+class Overview extends Component {}
 export default App;
